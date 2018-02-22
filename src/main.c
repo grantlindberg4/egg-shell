af96a0858fa868e2fe50cd992485873e3c8bc1fa
Christopher Feener <christopher.feener@wsu.edu>
2018-02-22 14:15:41 -0800

    Add getenv("PATH").

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdlib.h>	//For getenv()
 
 int main() {
     while(1) {
         char* line = readline("Enter a line: ");
         printf("%s\n", line);
+	char * pathName = getenv("PATH");
+	printf("PATH: %s\n", pathName);
     }
     return 0;
 }

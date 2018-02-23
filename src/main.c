190aed9209bba6a299e6700c84d48efbf2b446c3
Christopher Feener <christopher.feener@wsu.edu>
2018-02-22 16:15:08 -0800

    Add token management in while loop using strtok().

--- src/main.c
+++ src/main.c
@@ -2,13 +2,25 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdlib.h>	//For getenv()
+#include <string.h>	//For strtok()
 
 int main() {
-    while(1) {
-        char* line = readline("Enter a line: ");
-        printf("%s\n", line);
 	char * pathName = getenv("PATH");
 	printf("PATH: %s\n", pathName);
-    }
+	char ** tokens = (char **)malloc(128 * sizeof(char *));
+	int i = 0;
+	tokens[i] = strtok(pathName, ":");
+	while (i < 128) {
+		tokens[i] = (char *)malloc(sizeof(char *));
+		tokens[i] = strtok(NULL, ":");
+		if (tokens[i] == NULL) break;
+		printf("%s\n", tokens[i]);
+		i++;
+	}
+
+    /*while(1) {
+        *//*char* line = readline("Enter a line: ");
+        printf("%s\n", line);*//*
+    }*/
     return 0;
 }
